26dd086499cc620f4cd0edcef3bc611a1d7b62f0
hembert iregui <hiregui92@gmail.com>
2019-07-02 22:51:48 -0500

    [ADD] update albums for parent element

--- src/app/element-card/element-card.component.ts
+++ src/app/element-card/element-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { User, Album } from '../models';
+import { Router } from '@angular/router';
 
-import { AuthenticationService } from '../services';
+import { AuthenticationService, AlertService } from '../services';
 
 @Component({
   selector: 'app-element-card',
@@ -10,8 +11,15 @@ import { AuthenticationService } from '../services';
 })
 export class ElementCardComponent implements OnInit {
 	@Input() album: Album;
+  @Output() albumValueChange: EventEmitter<any>;
 	currentUser: User;
-  	constructor(private authenticationService: AuthenticationService) { }
+  	constructor(
+      private authenticationService: AuthenticationService,
+      private alertService: AlertService,
+      public router: Router
+      ) { 
+      this.albumValueChange = new EventEmitter();
+    }
 
   	ngOnInit() {
   		this.currentUser = this.authenticationService.currentUserValue;
@@ -22,27 +30,35 @@ export class ElementCardComponent implements OnInit {
   		if(this.currentUser.favorites) {
   			favorites = this.currentUser.favorites.split(',');
   		}
-		var a = favorites.indexOf(id);
-		if (a == -1) {
-			favorites.push(id);
-			var faStr = favorites.toString();
-			this.currentUser.favorites = faStr;
-			localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
-		}
+  		var a = favorites.indexOf(id);
+  		if (a == -1) {
+  			favorites.push(id);
+  			var faStr = favorites.toString();
+  			this.currentUser.favorites = faStr;
+        this.alertService.success('Elemento agregado satisfactoriamente a favoritos');
+  			localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+  		}
   	}
 
   	removeFavorite(id) {
+      console.log(id);
   		let favorites = [];
   		if(this.currentUser.favorites) {
   			favorites = this.currentUser.favorites.split(',');
   		}
-		var a = favorites.indexOf(id);
-		if (a !== -1) {
-			favorites.splice(a, 1);
-			var faStr = favorites.toString();
-			this.currentUser.favorites = faStr;
-			localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
-		}
+  		var a = favorites.indexOf(id);
+  		if (a !== -1) {
+        console.log('id');
+  			favorites.splice(a, 1);
+  			var faStr = favorites.toString();
+  			this.currentUser.favorites = faStr;
+        this.albumValueChange.emit({id: id, type: "delete"});
+        this.alertService.error('Elemento eliminado satisfactoriamente');
+  			localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+  		}
   	}
+  playAlbum(id) {
+    this.router.navigate(['album', id]);
+  }
 
 }
